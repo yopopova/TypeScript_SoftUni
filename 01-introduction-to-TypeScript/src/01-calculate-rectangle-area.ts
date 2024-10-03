912359b7ecbe6f269d8bf46dfcdff538722ceb3f
Yoanna Popova <yoanna.dp@gmail.com>
2024-10-03 09:57:28 +0300

    Add solution to the first task.

---
+++ 01-introduction-to-TypeScript/src/01-calculate-rectangle-area.ts
@@ -0,0 +1,7 @@
+function rectangleArea(recLength: number, recWidth: number): void {
+    const recArea = recLength * recWidth;
+    console.log(recArea);
+}
+
+rectangleArea(5, 7);
+rectangleArea(6, 8);
