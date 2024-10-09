403ce5dbae9dd5c2427d2f131be90c305a2fb449
Yoanna Popova <yoanna.dp@gmail.com>
2024-10-09 09:42:55 +0300

    Create a simple class with constructor and public properties for the first task.

---
+++ 03-OOP/src/01-car-info.ts
@@ -0,0 +1,13 @@
+class Car {
+    public _brand: string;
+    public _model: string;
+    public _horsepower: number;
+
+    constructor(brand: string, model: string, horsepower: number) {
+        this._brand = brand;
+        this._model = model;
+        this._horsepower = horsepower;
+    }
+
+    
+}
