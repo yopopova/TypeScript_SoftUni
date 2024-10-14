49f3b349863c9d33f90769265e4ba578acfb73b3
Yoanna Popova <yoanna.dp@gmail.com>
2024-10-14 09:05:55 +0300

    Create array from employees for fifth task.

--- 03-OOP/src/05-company-roster.ts
+++ 03-OOP/src/05-company-roster.ts
@@ -16,3 +16,59 @@ class Employee {
     }
 }
 
+function companyRoster(arr: string[]): void {
+    const arrLength = Number(arr.shift());
+
+    const employees: Employee[] = [];
+
+    for (let i = 0; i < arrLength; i++) {
+        const [name, salary, position, department, email, age] = arr[i].split(' ');
+
+        const empSalary = Number(salary);
+        const empAge = Number(age);
+
+        if (email !== undefined && age !== undefined) {
+            const newEmployee = new Employee(name, empSalary, position, department, email, empAge);
+            employees.push(newEmployee);
+            
+        } else if (email === undefined && age === undefined) {
+            const newEmployee = new Employee(name, empSalary, position, department);
+            employees.push(newEmployee);
+
+        } else if (Number(email) / 1) {
+            const currentEmail = 'n/a';
+            const currantAge = Number(email);
+
+            const newEmployee = new Employee(name, empSalary, position, department, currentEmail, currantAge);
+            employees.push(newEmployee);
+
+        } else {
+            const newEmployee = new Employee(name, empSalary, position, department, email);
+            employees.push(newEmployee);
+        }
+    }
+
+    // console.log(employees);
+
+    
+}
+
+companyRoster([
+    '4',
+    'Peter 120.00 Dev Development peter@abv.bg 28',
+    'Tina 333.33 Manager Marketing 33',
+    'Sam 840.20 ProjectLeader Development sam@sam.com',
+    'George 0.20 Freeloader Nowhere 18'
+]);
+
+console.log('----------------------------------------');
+
+companyRoster([
+    '6',
+    'Silver 496.37 Temp Coding silver@yahoo.com',
+    'Sam 610.13 Manager Sales',
+    'John 609.99 Manager Sales john@abv.bg 44',
+    'Venci 0.02 Director BeerDrinking beer@beer.br 23',
+    'Andre 700.00 Director Coding',
+    'Popeye 13.3333 Sailor SpinachGroup popeye@pop.ey'
+]);
