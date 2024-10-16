9117554da2f39674180302dfa87f7c489c22f60f
Yoanna Popova <yoanna.dp@gmail.com>
2024-10-16 12:38:39 +0300

    Create CarDealer class with its properties for the third task.

--- 04-generics/src/03-car-dealership.ts
+++ 04-generics/src/03-car-dealership.ts
@@ -8,3 +8,27 @@ interface Actions<T> {
     sellCar(dealerId: T, model: T): T;
 }
 
+class CarDealer<T> implements Dealership<T>, Actions<T> {
+    dealershipName: T;
+    soldCars?: number;
+    param?: T;
+    public modelsSold: { [key: string]: T };
+    // ^ Use an index signature because TypeScript cannot guarantee that 'this.modelsSold' has a specific structure, if we write just 'object'.
+
+    constructor (dealershipName: T, soldCars?: number, param?: T, modelsSold: { [key: string]: T } = {}) {
+        this.dealershipName = dealershipName;
+        this.soldCars = soldCars || 0;
+        this.param = param;
+        this.modelsSold = modelsSold;
+    }
+
+    
+}
+
+let dealership = new CarDealer('SilverStar');
+
+dealership.sellCar('BG01', 'C Class');
+dealership.sellCar('BG02', 'S Class');
+dealership.sellCar('BG03', 'ML Class');
+dealership.sellCar('BG04', 'CLK Class');
+console.log(dealership.showDetails());
