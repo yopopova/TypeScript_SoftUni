5016e76c9ee5cccbbc78bb895dee1c3ae8db5a2e
Yoanna Popova <yoanna.dp@gmail.com>
2024-10-18 09:28:54 +0300

    Change the way we invoke the last method.

--- 04-generics/src/03-car-dealership.ts
+++ 04-generics/src/03-car-dealership.ts
@@ -43,4 +43,4 @@ dealership.sellCar('BG01', 'C Class');
 dealership.sellCar('BG02', 'S Class');
 dealership.sellCar('BG03', 'ML Class');
 dealership.sellCar('BG04', 'CLK Class');
-console.log(dealership.showDetails());
+dealership.showDetails();
