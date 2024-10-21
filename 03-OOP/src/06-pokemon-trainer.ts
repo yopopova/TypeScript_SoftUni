246100510d49c38be671730cb019edab062c557e
Yoanna Popova <yoanna.dp@gmail.com>
2024-10-21 08:52:38 +0300

    Create trainer records with their pokemon collections.

--- 03-OOP/src/06-pokemon-trainer.ts
+++ 03-OOP/src/06-pokemon-trainer.ts
@@ -30,3 +30,36 @@ class Trainer {
     }
 }
 
+function pokemonTrainer(infoArr: string[]): void {
+    const trainers: { [name: string]: Trainer } = {};
+    
+    let index = 0;
+    let command = infoArr[index];
+
+    while (command !== 'Tournament') {
+        let [trainerName, pokemonName, pokemonElement, pokemonHealth] = command.split(' ');
+        let health = Number(pokemonHealth);
+
+        if (!trainers[trainerName]) {
+            trainers[trainerName] = new Trainer(trainerName);
+            trainers[trainerName].pokemonCollection.push(new Pokemon(pokemonName, pokemonElement, health));
+        } else {
+            trainers[trainerName].pokemonCollection.push(new Pokemon(pokemonName, pokemonElement, health));
+        }
+        
+        index++;
+        command = infoArr[index];
+    }
+    
+    
+}
+
+pokemonTrainer(
+    ['Peter Charizard Fire 100', 'George Squirtle Water 38', 'Peter Pikachu Electricity 10', 'Tournament', 'Fire', 'Electricity', 'End']
+);
+
+console.log('--------------------------------');
+
+pokemonTrainer(
+    ['Sam Blastoise Water 18', 'Narry Pikachu Electricity 22', 'John Kadabra Psychic 90', 'Tournament', 'Fire', 'Electricity', 'Fire', 'End']
+);
