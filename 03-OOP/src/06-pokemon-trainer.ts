4aea151219bd2bf0818cff8949325388707980bf
Yoanna Popova <yoanna.dp@gmail.com>
2024-10-21 08:51:23 +0300

    Create class Trainer for the sixth task.

--- 03-OOP/src/06-pokemon-trainer.ts
+++ 03-OOP/src/06-pokemon-trainer.ts
@@ -14,3 +14,19 @@ class Pokemon {
     }
 }
 
+class Trainer {
+    name: string;
+    badgesNumber?: number = 0;
+    pokemonCollection?: Pokemon[] = [];
+
+    constructor(name: string, badgesNumber?: number, pokemonCollection?: Pokemon[]) {
+        this.name = name;
+        this.badgesNumber = badgesNumber || 0;
+        this.pokemonCollection = pokemonCollection || [];
+    }
+
+    increaceBadges(): number {
+        return this.badgesNumber += 1;
+    }
+}
+
