52b4266fb5c7ea379cddb8b0cf3766159db5361b
Yoanna Popova <yoanna.dp@gmail.com>
2024-10-21 08:50:36 +0300

    Create class Pokemon for the sixth task.

---
+++ 03-OOP/src/06-pokemon-trainer.ts
@@ -0,0 +1,16 @@
+class Pokemon {
+    name: string;
+    element: string;
+    health: number;
+
+    constructor(name: string, element: string, health: number) {
+        this.name = name;
+        this.element = element;
+        this.health = health;
+    }
+
+    reduceHealth(): number {
+        return this.health -= 10;
+    }
+}
+
