59e00b2c6b3735076a79e44361d6117c9fa06d5a
Yoanna Popova <yoanna.dp@gmail.com>
2024-10-22 08:40:46 +0300

    Add solution to the third task.

--- 03-OOP/src/03-bank-account.ts
+++ 03-OOP/src/03-bank-account.ts
@@ -29,3 +29,53 @@ class BankAccount {
     }
 }
 
+function createAccounts(arr: string[]) {
+    const accounts: Map<number, BankAccount> = new Map();
+
+    let index = 0;
+    let line = arr[index];
+
+    while (line !== 'End') {
+        const [command, numOne, numTwo] = line.split(' ');
+
+        if (command === 'Create') {
+            const newAccount = new BankAccount();
+            accounts.set(newAccount.getId(), newAccount);
+
+        } else if (command === 'Deposit') {
+            const id = Number(numOne);
+            const amount = Number(numTwo);
+
+            const account = accounts.get(id);
+
+            if (account) {
+              account.deposit(amount);
+            } else {
+              console.log(`Account does not exist`);
+            }
+
+        } else if (command === 'SetInterest') {
+            const rate = Number(numOne);
+            BankAccount.setInterestRate(rate);
+
+        } else if (command === 'GetInterest') {
+            const id = Number(numOne);
+            const years = Number(numTwo);
+
+            const account = accounts.get(id);
+
+            if (account) {
+              const interest = account.getInterest(years);
+              console.log(interest.toFixed(2));
+            } else {
+              console.log(`Account does not exist`);
+            }
+        }
+        
+        index++;
+        line = arr[index];
+    }
+}
+
+// createAccounts(['Create', 'Deposit 1 20', 'GetInterest 1 10', 'End']);
+createAccounts(['Create', 'Create', 'Deposit 1 20', 'Deposit 3 20', 'Deposit 2 10', 'SetInterest 1.5', 'GetInterest 1 1', 'GetInterest 2 1', 'GetInterest 3 1', 'End']);
